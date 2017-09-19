439f8ae6cf4c9622cb8f202ac1302ea2abd74337
ZacZhang <zaczhang19@gmail.com>
2017-09-18 22:59:18 -0400

    news_api_client finished

---
+++ common/news_api_client.py
@@ -0,0 +1,35 @@
+import requests
+from json import loads
+
+NEWS_API_ENDPOINT = 'https://newsapi.org/v1/'
+NEWS_API_KEY = '569fb95a74414243b30c2bec25d8b7ff'
+ARTICLES_API = 'articles'
+
+CNN = 'cnn'
+DEFAULT_SOURCES = [CNN]
+SORT_BY_TOP = 'top'
+
+def buildUrl(end_point=NEWS_API_ENDPOINT, api_name=ARTICLES_API):
+	return end_point + api_name
+
+def getNewsFromSource(sources=DEFAULT_SOURCES, sortBy=SORT_BY_TOP):
+	articles = []
+	for source in sources:
+		payload = {'apiKey' : NEWS_API_KEY,
+				   'source' : source,
+				   'sortBy' : sortBy}
+		response = requests.get(buildUrl(), params=payload)
+		res_json = loads(response.content)
+
+		# extract info from response
+		if (res_json is not None and 
+			res_json['status'] == 'ok' and 
+			res_json['source'] is not None):
+
+			# populate news
+			for news in res_json['articles']:
+				news['source'] = res_json['source']
+
+			articles.extend(res_json['articles'])
+	return articles
+
