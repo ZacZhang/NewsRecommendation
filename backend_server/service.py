f24cd9cb27c8972149301aa8f1afb5adcf4dde85
ZacZhang <zaczhang19@gmail.com>
2017-09-20 18:01:07 -0400

    implemented pagination

--- backend_server/service.py
+++ backend_server/service.py
@@ -1,33 +1,31 @@
+import operations
 import pyjsonrpc
-import os
-import sys
-import json
-from bson.json_util import dumps
-
-# import common package in parent directory
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
-import mongodb_client
 
 SERVER_HOST = 'localhost'
 SERVER_PORT = 4040
 
 class RequestHandler(pyjsonrpc.HttpRequestHandler):
-	""" Test method"""
-	@pyjsonrpc.rpcmethod
-	def add(self, a, b):
-		print "add is called with %d and %d" % (a, b)
-		return a + b
+    """ Test method """
+    @pyjsonrpc.rpcmethod
+    def add(self, a, b):
+        print "add is called with %d and %d" % (a, b)
+        return a + b
 
-	@pyjsonrpc.rpcmethod
-	def getNews(self):
-		db = mongodb_client.get_db()
-		news = list(db['news'].find())
-		return json.loads(dumps(news))
+    """ Get news summaries for a user """
+    @pyjsonrpc.rpcmethod
+    def getNewsSummariesForUser(self, user_id, page_num):
+        return operations.getNewsSummariesForUser(user_id, page_num)
 
+    """ Log user news clicks """
+    @pyjsonrpc.rpcmethod
+    def logNewsClickForUser(self, user_id, news_id):
+        return operations.logNewsClickForUser(user_id, news_id)
 
+# Threading HTTP Server
 http_server = pyjsonrpc.ThreadingHttpServer(
-	server_address = (SERVER_HOST, SERVER_PORT),
-	RequestHandlerClass = RequestHandler)
+    server_address = (SERVER_HOST, SERVER_PORT),
+    RequestHandlerClass = RequestHandler
+)
 
 print "Starting HTTP server on %s:%d" % (SERVER_HOST, SERVER_PORT)
 
--- web_server/client/src/Login/LoginForm.js
+++ web_server/client/src/Login/LoginForm.js
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import { Link } from 'react-router';
 import './LoginForm.css';
 
 const LoginForm = ({
@@ -30,7 +31,7 @@ const LoginForm = ({
                     <input type="submit" className="waves-effect waves-light btn indigo lighten-1" value='Log in'/>
                 </div>
                 <div className="row">
-                    <p className="right-align"> New user?  <a href="/signup">Sign Up</a></p>
+                    <p className="right-align"> New user?  <Link to="/signup">Sign Up</Link></p>
                 </div>
             </form>
         </div>
